b17a37e775b4f28ebea7332e724edb6c8e3d5005
trangmx <trangmx@gmail.com>
2015-02-26 12:14:48 +0900

    Minor edit

--- jp.go.nict.langrid.servicemanager.web/src/jp/go/nict/langrid/management/web/view/page/user/component/text/TermofUseField.java
+++ jp.go.nict.langrid.servicemanager.web/src/jp/go/nict/langrid/management/web/view/page/user/component/text/TermofUseField.java
@@ -1,14 +1,10 @@
 package jp.go.nict.langrid.management.web.view.page.user.component.text;
 
-import jp.go.nict.langrid.management.web.view.component.validator.URLValidator;
-
 import org.apache.wicket.markup.html.form.TextArea;
-import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
 public class TermofUseField extends TextArea<String>{
-	
 	public TermofUseField(String componentId, Model<String> model){
 		super(componentId, model);
 	}
@@ -20,4 +16,6 @@ public class TermofUseField extends TextArea<String>{
 	public TermofUseField(String componentId, IModel<String> model){
 		super(componentId, model);
 	}
+
+	private static final long serialVersionUID = 8459987228973946705L;
 }
