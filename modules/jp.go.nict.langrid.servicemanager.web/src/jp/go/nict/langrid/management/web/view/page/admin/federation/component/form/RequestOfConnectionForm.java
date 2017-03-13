aea7ec65255f2c3ff29206f58b5faeb2462002b0
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2017-03-13 10:56:27 +0900

    exstract method.

--- modules/jp.go.nict.langrid.servicemanager.web/src/jp/go/nict/langrid/management/web/view/page/admin/federation/component/form/RequestOfConnectionForm.java
+++ modules/jp.go.nict.langrid.servicemanager.web/src/jp/go/nict/langrid/management/web/view/page/admin/federation/component/form/RequestOfConnectionForm.java
@@ -40,6 +40,7 @@ import jp.go.nict.langrid.management.web.view.component.text.RequiredURLField;
 import jp.go.nict.langrid.management.web.view.page.admin.federation.AllOperatorsPage;
 import jp.go.nict.langrid.management.web.view.page.user.component.text.RequiredUserIdField;
 import jp.go.nict.langrid.repackaged.net.arnx.jsonic.JSON;
+import jp.go.nict.langrid.service_1_2.ProcessFailedException;
 
 /**
  * 
@@ -73,101 +74,11 @@ public abstract class RequestOfConnectionForm extends AbstractForm<String> {
 		add(new Button("submit") {
 			@Override
 			public void onSubmit() {
-				String token = FederationLogic.newToken();
-				
-				String inputedUrl = url.getModelObject();
-				String[] hostAndPort = inputedUrl.split("/", 4)[2].split(":");
-				
-				String host = hostAndPort[0];
-				int port = 1 < hostAndPort.length ? Integer.valueOf(hostAndPort[1]) : 80;
-				
-				String requestUrl = inputedUrl.endsWith("/") ? inputedUrl : inputedUrl + "/";
-				requestUrl += "federation/request";
-				
-				try {
-					HttpClient hc = HttpClientUtil.createHttpClientWithHostConfig(new URL(requestUrl));
-					SimpleHttpConnectionManager manager = new SimpleHttpConnectionManager(true);
-					manager.getParams().setConnectionTimeout(20000);
-					manager.getParams().setSoTimeout(20000);
-					hc.setHttpConnectionManager(manager);
-					GridModel grid = ServiceFactory.getInstance().getGridService().get(gridId);
-					FederationRequest request = new FederationRequest();
-					request.setToken(token);
-					request.setRequestUserId(userId.getModelObject());
-					request.setSourceGrid(grid);
-					request.setSourceUrl(grid.getUrl());
-					PostMethod pm = new PostMethod(requestUrl);
-					pm.setParameter("request", JSON.encode(request));
-					pm.setParameter("requestType", RequestType.CONNECT.name());
-					pm.setDoAuthentication(true);
-					hc.getState().setCredentials(
-						new AuthScope(host, port, null)
-						,new UsernamePasswordCredentials(
-							userId.getModelObject(), password.getModelObject())
-						);
-					hc.getParams().setAuthenticationPreemptive(true);
-					int code = hc.executeMethod(pm);
-					if(code == HttpStatus.SC_OK) {
-						String res = pm.getResponseBodyAsString();
-						targetGridId = requestUrl;
-						if(res != null) {
-							FederationResponse fr = JSON.decode(res, FederationResponse.class);
-							targetGridId = fr.getTargetGrid().getGridId();
-							if(targetGridId == null) {
-								targetGridId = requestUrl;
-							}
-							GridService gService = ServiceFactory.getInstance().getGridService();
-							if(fr.isApproved() && gService.get(fr.getTargetGrid().getGridId()) == null) {
-								fr.getTargetGrid().setHosted(false);
-								gService.add(fr.getTargetGrid());
-								LogWriter.writeInfo("Operator"
-									, MessageManager.getMessage(
-										"LanguageGridOperator.federation.log.connect.registration.Grid"
-										, Locale.ENGLISH, targetGridId)
-									, getClass());
-								try {
-									// add news
-									NewsModel nm = new NewsModel();
-									nm.setGridId(gridId);
-									Map<String, String> map = new HashMap<String, String>();
-									map.put("gridId", targetGridId);
-									nm.setContents(MessageManager.getMessage("news.federation.Connected", map));
-									// set node id
-									nm.setNodeId(MessageUtil.getSelfNodeId());
-									ServiceFactory.getInstance().getNewsService(gridId).add(nm);
-								} catch(ServiceManagerException e) {
-									LogWriter.writeError("Operator", e, getClass());
-								}
-							}
-							FederationService fService = ServiceFactory.getInstance().getFederationService(gridId);
-							FederationModel fm = fService.get(gridId, targetGridId);
-							if(fm == null) {
-								fm = new FederationModel();
-								fm.setRequesting(!fr.isApproved());
-								fm.setConnected(fr.isApproved());
-								fm.setSourceGridId(gridId);
-								fm.setSourceGridName(grid.getGridName());
-								fm.setTargetGridId(targetGridId);
-								fm.setTargetGridName(fr.getTargetGrid().getGridName());
-								fm.setTargetGridAccessToken(token);
-								fm.setTargetGridUserHomepage(new URL(fr.getOperatorHomepage()));
-								fm.setTargetGridUserId(userId.getModelObject());
-								fm.setTargetGridUserOrganization(fr.getOperatorOrganization());
-								fService.add(fm);
-							} else if(fr.isApproved()){
-								fService.setRequesting(gridId, targetGridId, false);
-								fService.setConnected(gridId, targetGridId, true);
-							}
-						}
-					} else if(code == HttpStatus.SC_INTERNAL_SERVER_ERROR
-						|| code == HttpStatus.SC_NOT_FOUND
-						|| code == HttpStatus.SC_FORBIDDEN
-						|| code == HttpStatus.SC_UNAUTHORIZED
-						|| code == HttpStatus.SC_SERVICE_UNAVAILABLE)
-					{
-						error(pm.getResponseBodyAsString());
-						setIsValidateError(true);
-					}
+				try{
+					requestFederation(
+							gridId,
+							url.getModelObject(), userId.getModelObject(),
+							password.getModelObject());
 				} catch(MalformedURLException e) {
 					doErrorProcess(new ServiceManagerException(e));
 				} catch(ConnectTimeoutException e) {
@@ -179,6 +90,9 @@ public abstract class RequestOfConnectionForm extends AbstractForm<String> {
 					doErrorProcess(new ServiceManagerException(e));
 				} catch(ServiceManagerException e) {
 					doErrorProcess(e);
+				} catch(ProcessFailedException e){
+					error(e.getDescription());
+					setIsValidateError(true);
 				}
 			}
 		});
@@ -201,6 +115,105 @@ public abstract class RequestOfConnectionForm extends AbstractForm<String> {
 		return targetGridId;
 	}
 
+	private static String requestFederation(
+			String selfGridId,
+			String url, String userId, String password)
+	throws ServiceManagerException, HttpException, IOException, ProcessFailedException{
+		String token = FederationLogic.newToken();
+		
+		String inputedUrl = url;
+		String[] hostAndPort = inputedUrl.split("/", 4)[2].split(":");
+		
+		String host = hostAndPort[0];
+		int port = 1 < hostAndPort.length ? Integer.valueOf(hostAndPort[1]) : 80;
+		
+		String requestUrl = inputedUrl.endsWith("/") ? inputedUrl : inputedUrl + "/";
+		requestUrl += "federation/request";
+		
+		HttpClient hc = HttpClientUtil.createHttpClientWithHostConfig(new URL(requestUrl));
+		SimpleHttpConnectionManager manager = new SimpleHttpConnectionManager(true);
+		manager.getParams().setConnectionTimeout(20000);
+		manager.getParams().setSoTimeout(20000);
+		hc.setHttpConnectionManager(manager);
+		GridModel grid = ServiceFactory.getInstance().getGridService().get(selfGridId);
+		FederationRequest request = new FederationRequest();
+		request.setToken(token);
+		request.setRequestUserId(userId);
+		request.setSourceGrid(grid);
+		request.setSourceUrl(grid.getUrl());
+		PostMethod pm = new PostMethod(requestUrl);
+		pm.setParameter("request", JSON.encode(request));
+		pm.setParameter("requestType", RequestType.CONNECT.name());
+		pm.setDoAuthentication(true);
+		hc.getState().setCredentials(
+			new AuthScope(host, port, null),
+			new UsernamePasswordCredentials(userId, password)
+			);
+		hc.getParams().setAuthenticationPreemptive(true);
+		int code = hc.executeMethod(pm);
+		String targetGridId = null;
+		if(code == HttpStatus.SC_OK) {
+			String res = pm.getResponseBodyAsString();
+			targetGridId = requestUrl;
+			if(res != null) {
+				FederationResponse fr = JSON.decode(res, FederationResponse.class);
+				targetGridId = fr.getTargetGrid().getGridId();
+				if(targetGridId == null) {
+					targetGridId = requestUrl;
+				}
+				GridService gService = ServiceFactory.getInstance().getGridService();
+				if(fr.isApproved() && gService.get(fr.getTargetGrid().getGridId()) == null) {
+					fr.getTargetGrid().setHosted(false);
+					gService.add(fr.getTargetGrid());
+					LogWriter.writeInfo("Operator"
+						, MessageManager.getMessage(
+							"LanguageGridOperator.federation.log.connect.registration.Grid"
+							, Locale.ENGLISH, targetGridId)
+						, RequestOfConnectionForm.class);
+					try {
+						// add news
+						NewsModel nm = new NewsModel();
+						nm.setGridId(selfGridId);
+						Map<String, String> map = new HashMap<String, String>();
+						map.put("gridId", targetGridId);
+						nm.setContents(MessageManager.getMessage("news.federation.Connected", map));
+						// set node id
+						nm.setNodeId(MessageUtil.getSelfNodeId());
+						ServiceFactory.getInstance().getNewsService(selfGridId).add(nm);
+					} catch(ServiceManagerException e) {
+						LogWriter.writeError("Operator", e, RequestOfConnectionForm.class);
+					}
+				}
+				FederationService fService = ServiceFactory.getInstance().getFederationService(selfGridId);
+				FederationModel fm = fService.get(selfGridId, targetGridId);
+				if(fm == null) {
+					fm = new FederationModel();
+					fm.setRequesting(!fr.isApproved());
+					fm.setConnected(fr.isApproved());
+					fm.setSourceGridId(selfGridId);
+					fm.setSourceGridName(grid.getGridName());
+					fm.setTargetGridId(targetGridId);
+					fm.setTargetGridName(fr.getTargetGrid().getGridName());
+					fm.setTargetGridAccessToken(token);
+					fm.setTargetGridUserHomepage(new URL(fr.getOperatorHomepage()));
+					fm.setTargetGridUserId(userId);
+					fm.setTargetGridUserOrganization(fr.getOperatorOrganization());
+					fService.add(fm);
+				} else if(fr.isApproved()){
+					fService.setRequesting(selfGridId, targetGridId, false);
+					fService.setConnected(selfGridId, targetGridId, true);
+				}
+			}
+		} else if(code == HttpStatus.SC_INTERNAL_SERVER_ERROR
+			|| code == HttpStatus.SC_NOT_FOUND
+			|| code == HttpStatus.SC_FORBIDDEN
+			|| code == HttpStatus.SC_UNAUTHORIZED
+			|| code == HttpStatus.SC_SERVICE_UNAVAILABLE)
+		{
+			throw new ProcessFailedException(pm.getResponseBodyAsString());
+		}
+		return targetGridId;
+	}
 	private RequiredURLField url;
 	private RequiredPasswordTextField confirm;
 	private RequiredPasswordTextField password;
