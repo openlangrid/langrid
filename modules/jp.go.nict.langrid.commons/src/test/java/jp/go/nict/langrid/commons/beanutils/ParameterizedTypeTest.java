c52d1ca28788ca42c2a2bccd82f8e989e020d44e
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2019-12-25 11:15:33 +0900

    add test for TypeReference.

--- modules/jp.go.nict.langrid.commons/src/test/java/jp/go/nict/langrid/commons/beanutils/ParameterizedTypeTest.java
+++ modules/jp.go.nict.langrid.commons/src/test/java/jp/go/nict/langrid/commons/beanutils/ParameterizedTypeTest.java
@@ -17,19 +17,81 @@
  */
 package jp.go.nict.langrid.commons.beanutils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import jp.go.nict.langrid.commons.lang.TypeReference;
 import jp.go.nict.langrid.repackaged.net.arnx.jsonic.JSON;
 
 public class ParameterizedTypeTest {
 	public List<String> hello(){ return null;}
-	public static void main(String[] args) throws Exception{
-		List<String> ret = new Converter().convert(
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void test() throws Throwable{
+		Object r = new Converter().convert(
 				JSON.decode("[\"hello\",\"world\"]")
 				, ParameterizedTypeTest.class.getMethod("hello").getGenericReturnType()
 				);
-		for(String s : ret){
-			System.out.println(s);
+		Assert.assertTrue(r instanceof List);
+		List<String> ret = (List<String>)r;
+		Assert.assertEquals("hello", ret.get(0));
+		Assert.assertEquals("world", ret.get(1));
+	}
+
+	static class User1{
+		private String name;
+		public User1() {
 		}
+		public User1(String name) {
+			super();
+			this.name = name;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+	public static class User2{
+		private String name;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+	@Test
+	public void test2() throws Throwable{
+		new Converter().convert(new User1("bob"), User2.class);
+	}
+	@Test
+	public void test3() throws Throwable{
+		List<User1> u1 = Arrays.asList(new User1("bob"));
+		Collection<User2> u2 = new Converter().convertCollection(u1, List.class, User2.class);
+		Assert.assertEquals("bob", u2.iterator().next().getName());
+	}
+
+	@SuppressWarnings("serial")
+	public static class User2List extends ArrayList<User2>{}
+	@Test
+	public void test4() throws Throwable{
+		List<User1> u1 = Arrays.asList(new User1("bob"));
+		Collection<User2> u2 = new Converter().convert(u1, User2List.class.getGenericSuperclass());
+		Assert.assertEquals("bob", u2.iterator().next().getName());
+	}
+
+	@Test
+	public void test5() throws Throwable{
+		List<User1> u1 = Arrays.asList(new User1("bob"));
+		Collection<User2> u2 = new Converter().convert(u1, new TypeReference<List<User2>>(){}.get());
+		Assert.assertEquals("bob", u2.iterator().next().getName());
 	}
 }
